4b34f706561093554334a09c6b96cf2fe2ac4e1c
Joseph2912 <elsjoseph@hotmail.com>
2024-04-22 21:04:46 -0500

    avance

--- Portafolio/src/componentes/Web.jsx
+++ Portafolio/src/componentes/Web.jsx
@@ -2,8 +2,18 @@ import "../Styles/Web.css";
 
 const Web = () => {
   return (
-    <div className="flex w-screen" id="web">
-      <p>hola mundo</p>
+    <div className="" id="web">
+      <h2 className="mt-10" id="developer">
+        WEB DEVELOPER
+      </h2>
+      <div>
+        <p className="mt-10" id="parrafo">
+          Como desarrollador web con experiencia en la creación de páginas web,
+          trabajo con tecnologías como React.js, Firebase, Tailwind y Bootstrap.
+          Además, poseo conocimientos en interfaces intuitivas, certificado por
+          la Interaction Design Foundation.
+        </p>
+      </div>
     </div>
   );
 };
